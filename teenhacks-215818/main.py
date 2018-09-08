4e6b41cc8838cd050a32d37421266caef0ec0740
Shayna Chambless <shaychamblessshay@gmail.com>
2018-09-08 11:11:18 -0700

    added jinja2

--- teenhacks-215818/main.py
+++ teenhacks-215818/main.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
+import webapp2, jinja2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
